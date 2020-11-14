0d50b77d8b7f78071ac83a6de1dcb9c1c68e5ea5
KharThi <khathi260100@gmail.com>
2020-11-14 14:17:23 +0700

    2nd

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -26,6 +26,7 @@ public class MathUtil {
 
         //Test Nhiều thứ khác ...(Lý thuyết là phải hết các trường hợp)
         //Test các ngoại lệ ...
+        System.out.println("Yeah!");
     }
 
 }
